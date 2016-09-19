5b2b5626a2ad934b727934c64ea82ebadbdf8d3a
Kristine <kteich88@gmail.com>
2016-09-19 10:43:41 -0400

    Adjusted code so that number statistics print when program breaks

--- user_input_stat_epic.rb
+++ user_input_stat_epic.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class String
   def is_string?
     self == self
@@ -20,53 +21,112 @@ class String
   end
 end
 
-input_arr = []
-
-puts "Enter a number or a string: "
-input = gets.chomp
-
-if (input.to_i.to_s == input) || (input.to_f.to_s == input) # checks if number
-  input_arr << input.to_f
-else
-  input_arr << input
+class Float
+  def sqrt
+    Math.sqrt(self)
+  end
 end
 
-loop do
-  puts "Enter a number or a string: "
-  input = gets.chomp
+def is_even?
+  return true
+end
 
-  if input == ""
-    break
+def get_sum(numbers)
+  total = 0
+  numbers.each do |number|
+    total += number
   end
+  total
+end
 
-  if !input.is_numeric? && !input.is_float? && !input.is_integer?
-    if input.is_numeric? #checks for numeric
-      input_arr << input.to_f
-    else
-      puts "Please Try Again! Type ruby user_input_stat_advanced.rb to rerun the program."
-      break
-    end
+def get_average(numbers)
+  get_sum(numbers) / numbers.count
+end
+
+def get_mode(numbers)
+  hash = Hash.new(0)
+  numbers.each do |frequent|
+    hash[frequent] += 1
+  end
+  return hash
+end
+
+def get_median(numbers)
+  sorted = numbers.sort
+  middle = numbers.length
+  if middle % 2 != 0
+    (middle + 1) / 2.0
   else
-    if !input.is_numeric? #|| !input.is_float? || !input.is_integer? #checks for string
-      input_arr << input
+    (((middle.length/2.0) + ((middle.length + 1)/2.0)) / 2.0)
+  end
+  return middle
+end
+
+def get_standard_diviation(numbers)
+  variance = numbers.sort
+  standard_diviation = Math.sqrt(variance.max - variance.min)
+  return standard_diviation
+end
+
+def try_again()
+  puts "Invalid input. Would you like to try again?"
+  print "Type \'retry\' to try again or \'exit\' to return to the Terminal: "
+  input = gets.chomp.downcase
+  if input == "retry"
+    main
+  else
+    exit
+  end
+end
+
+def main()
+  elements = []
+  puts "Enter a number OR a 'string' value: "
+  user_input = gets.chomp
+    if (user_input.to_i.to_s == user_input) || (user_input.to_f.to_s == user_input)
+      elements << user_input.to_f
     else
-      puts "Please Try Again! Type ruby user_input_stat_advanced.rb to rerun the program."
-      break
+      elements << user_input
+    end
+
+  loop do
+    puts "Enter a number OR a 'string' value: "
+    user_input = gets.chomp
+
+    if (user_input.to_i.to_s == user_input) || (user_input.to_f.to_s == user_input)
+      elements.include?(user_input.is_a? Float)
+      elements << user_input.to_f
+
+    elsif elements.include?(user_input.is_a? String)
+      elements << user_input
+
+    else
+      if elements.include?(user_input.is_a? String) && elements.include?(user_input.is_a? Float)
+        try_again()
+      end
+      if user_input == ""
+        if elements.include?(user_input.is_a? String)
+          puts elements.join
+          puts elements.count
+          puts elements.length.min
+          puts elements.length.max
+          puts elements.length / elements.count
+        # The number of times the letter e was used in the inputs.
+          exit
+        else
+          puts "Count: #{elements.count}"
+          puts "Sum: #{get_sum(elements)}"
+          puts "Average/Mean: #{get_average(elements)}"
+          puts "Median: #{get_median(elements)}"
+          puts "Mode: #{get_mode(elements)}"
+          puts "Standard Deviation: #{get_standard_diviation(elements)}"
+          exit
+        end
+      end
     end
   end
 end
 
-if input_arr.include?(!input.is_numeric?)
-  puts input_arr.join
-else
-  puts "Count: #{input_arr.count}"
-  sum = 0
-  input_arr.each do |number|
-    sum += number
-  end #sum
-  puts "Sum: #{sum}"
-  average = sum / input_arr.count #average
-  puts "Average: #{average}"
+if __FILE__ == $PROGRAM_NAME
+main
 end
-
-#p user_input_stat (to print array for check)
