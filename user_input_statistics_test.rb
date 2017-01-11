80816dcd886ddaeb3a7778c17150822a1cdb3d04
Kristine <kteich88@gmail.com>
2017-01-11 14:40:30 -0500

    Created Minitest file and first test.

---
+++ user_input_statistics_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/nyan_cat'
+require_relative 'user_input_statistics'
+
+#
+class TestUserInputStatistics < Minitest::Test
+  def test_is_numeric?
+    assert_equal true, is_numeric?('42')
+    assert_equal true, is_numeric?('3.45')
+    assert_equal true, is_numberic?('-9')
+    assert_equal true, is_numberic?('-1.23')
+    assert_equal false, is_numeric?('kitten')
+  end
+end
