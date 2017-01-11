c69893f7e2f09a9208901df29b53757ce6816fae
Kristine <kteich88@gmail.com>
2017-01-11 14:48:58 -0500

    Corrected typos in test file.

--- user_input_statistics_test.rb
+++ user_input_statistics_test.rb
@@ -7,8 +7,8 @@ class TestUserInputStatistics < Minitest::Test
   def test_is_numeric?
     assert_equal true, is_numeric?('42')
     assert_equal true, is_numeric?('3.45')
-    assert_equal true, is_numberic?('-9')
-    assert_equal true, is_numberic?('-1.23')
+    assert_equal true, is_numeric?('-9')
+    assert_equal true, is_numeric?('-1.23')
     assert_equal false, is_numeric?('kitten')
   end
 end
