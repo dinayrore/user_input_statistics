25249b922b1fc4dddcd1399cd4716f30e9fc1409
Kristine <kteich88@gmail.com>
2017-01-11 15:16:36 -0500

    Added remainder of test suite test names.

--- user_input_statistics_test.rb
+++ user_input_statistics_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'minitest/nyan_cat'
 require_relative 'user_input_statistics'
 
-#
+# Minitest suite for User Input Statistics Normal Mode.
 class TestUserInputStatistics < Minitest::Test
   def test_is_numeric?
     assert_equal true, is_numeric?('42')
@@ -10,5 +10,21 @@ class TestUserInputStatistics < Minitest::Test
     assert_equal true, is_numeric?('-9')
     assert_equal true, is_numeric?('-1.23')
     assert_equal false, is_numeric?('kitten')
+  end
+
+  def test_push_to_array
+    assert
+  end
+
+  def test_count
+
+  end
+
+  def test_sum
+
+  end
+
+  def test_average
+
   end
 end
