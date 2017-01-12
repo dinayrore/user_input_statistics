987b0dc36b01549bb26e18077c0957ab3ee3487f
Kristine <kteich88@gmail.com>
2017-01-11 20:17:57 -0500

    Attempting another approach, stuck on Minitests.

--- user_input_statistics.rb
+++ user_input_statistics.rb
@@ -1,39 +1,28 @@
-#
-class UserInputStatistics
-  def initialize(value)
-    @value = value
-  end
+def user_input
+  puts 'Enter a number: '
+  value = gets.chomp
+end
 
-
-  def numeric?(value)
-    if value.to_i.to_s == value || value.to_f.to_s == value
-      true
-      #TODO: method to push values into array
-    else
-      false
-      #TODO: method to inform user of invalid input
-    end
+def numeric?(value, input)
+  if value.to_i.to_s == value || value.to_f.to_s == value
+    input << value
+  else
+    puts 'Invalid input '
   end
+end
 
 # def store_value
 # end
 #
-# def input_invalid
-# end
+
 #
 # def count()
 # end
 
-# user_input_stat = []
-# num = nil
+
 #
-# while true
-#     puts "enter a number: "
-#     num = gets.chomp
 #   if (num.to_i.to_s == num)|| (num.to_f.to_s == num)
 #     user_input_stat << num
-#   elsif num == ""
-#     break
 #   else #(num.to_i.to_s != num)|| (num.to_f.to_s != num)
 #     print "invalid input "
 #   end
@@ -49,13 +38,12 @@ class UserInputStatistics
 # puts "Average: #{average}"
 
 def main
+  input = []
+  value = nil
   until value == ''
-    puts 'Enter a number: '
-    value = gets.chomp
-    UserInputStatistics.new(value)
+    value = user_input
+    numeric?(value, input)
   end
-  value = user_input
-  numeric?(value)
 end
 
 main if __FILE__ == $PROGRAM_NAME
