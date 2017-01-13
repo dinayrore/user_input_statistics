cceb20457fbd159ac5615446ef4d79cf12b1e24d
Kristine <kteich88@gmail.com>
2017-01-12 22:50:32 -0500

    Updated Minitests for numeric function.

--- user_input_statistics_test.rb
+++ user_input_statistics_test.rb
@@ -5,20 +5,20 @@ require_relative 'user_input_statistics'
 # Minitest suite for User Input Statistics Normal Mode.
 class TestUserInputStatistics < Minitest::Test
   def test_numeric?
-    assert_equal true, numeric?('42')
-    assert_equal true, numeric?('3.45')
-    assert_equal true, numeric?('-9')
-    assert_equal true, numeric?('-1.23')
-    assert_equal false, numeric?('kitten')
+    assert_equal ['42'], numeric?('42', [])
+    assert_equal ['3.45'], numeric?('3.45', [])
+    assert_equal ['-9'], numeric?('-9', [])
+    assert_equal ['-1.23'], numeric?('-1.23', [])
+    assert_output('Invalid input. ') { print 'Invalid input. ' } if numeric?('kitten', [])
   end
 
-  # def test_count
-  #   assert_empty([])
-  #   assert_equal(1, count(['42']))
-  #   assert_equal(2, count(['42', '3.45']))
-  #   assert_equal(3, count(['42', '3.45', '-9']))
-  #   assert_equal(4, count(['42', '3.45', '-9', '-1.23']))
-  # end
+  def test_count
+    assert_empty([])
+    assert_equal(1, count(['42']))
+    assert_equal(2, count(['42', '3.45']))
+    assert_equal(3, count(['42', '3.45', '-9']))
+    assert_equal(4, count(['42', '3.45', '-9', '-1.23']))
+  end
   #
   # def test_sum
   #
