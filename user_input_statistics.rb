c819c7d9d9fd519476573867a0eb8644a07b85d7
Kristine <kteich88@gmail.com>
2017-01-16 18:08:16 -0500

    Fixed error in calculating sum & average.

--- user_input_statistics.rb
+++ user_input_statistics.rb
@@ -8,18 +8,17 @@ def numeric?(value, input)
   if value.to_i.to_s == value || value.to_f.to_s == value
     input << value
     user_input(input)
-  elsif value == "\n"
+  elsif value == ''
     print_statistics(input)
   else
     print 'Invalid input. '
     user_input(input)
   end
-  input
 end
 
 def calculate_sum(input)
   sum = 0
-  input.each { |v| sum += v.to_f }
+  input.inject(0){|sum,n| sum + n.to_f }
 end
 
 def calculate_average(sum, input)
