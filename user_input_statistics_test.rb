902381f93b7a657402d0e4acf77841df32a403f2
Kristine <kteich88@gmail.com>
2017-01-16 22:53:30 -0500

    Completed Minitests, all passing.

--- user_input_statistics_test.rb
+++ user_input_statistics_test.rb
@@ -9,22 +9,22 @@ class TestUserInputStatistics < Minitest::Test
     assert_equal ['3.45'], numeric?('3.45', [])
     assert_equal ['-9'], numeric?('-9', [])
     assert_equal ['-1.23'], numeric?('-1.23', [])
+  end
+
+  def test_validate_input
     assert_output('Invalid input. ') { print 'Invalid input. ' } if numeric?('kitten', [])
   end
 
-  def test_count
-    assert_empty([])
-    assert_equal(1, count(['42']))
-    assert_equal(2, count(['42', '3.45']))
-    assert_equal(3, count(['42', '3.45', '-9']))
-    assert_equal(4, count(['42', '3.45', '-9', '-1.23']))
+  def test_calculate_sum
+    assert_equal(0, calculate_sum(['0']))
+    assert_equal(42, calculate_sum(['42']))
+    assert_equal(33, calculate_sum(['42', '-9']))
+    assert_equal(35.22, calculate_sum(['42', '3.45', '-9', '-1.23']))
   end
-  #
-  # def test_sum
-  #
-  # end
-  #
-  # def test_average
-  #
-  # end
+
+  def test_calculate_average
+    assert_equal(0, calculate_average(0, ['0']))
+    assert_equal(16, calculate_average(33, ['42', '-9']))
+    assert_equal(8.805, calculate_average(35.22, ['42', '3.45', '-9', '-1.23']))
+  end
 end
