d598c04d37af4a23f8140c8be6e5755dd69d76e2
Kristine <kteich88@gmail.com>
2017-01-17 11:37:17 -0500

    Added first method tests for numeric function.

--- spec/user_input_statistics_epic_spec.rb
+++ spec/user_input_statistics_epic_spec.rb
@@ -1,4 +1,26 @@
 require 'user_input_statistics_epic'
 
-describe UserInputStatisticsEpic do
+describe 'User Input Statistics Epic Mode' do
+  describe '#numeric' do
+    context "given '42', []" do
+      it "returns ['42']" do
+        expect(numeric('42', [])).to eq(['42'])
+      end
+    end
+    context "given '3.45', []" do
+      it "returns ['3.45']" do
+        expect(numeric('3.45', [])).to eq(['3.45'])
+      end
+    end
+    context "given '-9', []" do
+      it "returns ['-9']" do
+        expect(numeric('-9', [])).to eq(['-9'])
+      end
+    end
+    context "given '-1.23', []" do
+      it "returns ['-1.23']" do
+        expect(numeric('-1.23', [])).to eq(['-1.23'])
+      end
+    end
+  end
 end
