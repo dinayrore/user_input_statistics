a43301a03a498a2aa6251e2efdd4d4f71797aa51
Kristine <kteich88@gmail.com>
2017-01-17 14:12:50 -0500

    Updated test file to test method string?

--- spec/user_input_statistics_epic_spec.rb
+++ spec/user_input_statistics_epic_spec.rb
@@ -4,22 +4,29 @@ describe 'User Input Statistics Epic Mode' do
   describe '#numeric?' do
     context "given '42', []" do
       it "returns ['42']" do
-        expect(numeric('42', [])).to eq(['42'])
+        expect(numeric?('42', [])).to eq(['42'])
       end
     end
     context "given '3.45', []" do
       it "returns ['3.45']" do
-        expect(numeric('3.45', [])).to eq(['3.45'])
+        expect(numeric?('3.45', [])).to eq(['3.45'])
       end
     end
     context "given '-9', []" do
       it "returns ['-9']" do
-        expect(numeric('-9', [])).to eq(['-9'])
+        expect(numeric?('-9', [])).to eq(['-9'])
       end
     end
     context "given '-1.23', []" do
       it "returns ['-1.23']" do
-        expect(numeric('-1.23', [])).to eq(['-1.23'])
+        expect(numeric?('-1.23', [])).to eq(['-1.23'])
+      end
+    end
+  end
+  describe '#string?' do
+    context "given 'kitten', []" do
+      it "returns ['kitten']" do
+        expect(string?('kitten', [])).to eq(['kitten'])
       end
     end
   end
