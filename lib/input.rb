ff0749585da569370255d3b62f42e1faab40bfe3
Kristine <kteich88@gmail.com>
2017-01-19 15:23:22 -0500

    Created input class.

---
+++ lib/input.rb
@@ -0,0 +1,18 @@
+# Determine type of user input
+class Input
+  def self.string?(value, array)
+    array << value if value.to_i.to_s != value || value.to_f.to_s != value
+  end
+
+  def self.float?(value)
+    return true if value.to_f.to_s == value
+  end
+
+  def self.integer?(value)
+    return true if value.to_i.to_s == value
+  end
+
+  def self.numeric?(value, array)
+    array << value if float?(value) || integer?(value)
+  end
+end
