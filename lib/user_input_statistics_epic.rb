b1645e16e1ff3037f4cddd3b7d2faa222d05ea0f
Kristine <kteich88@gmail.com>
2017-01-19 15:21:41 -0500

    Removed class related code.

--- lib/user_input_statistics_epic.rb
+++ lib/user_input_statistics_epic.rb
@@ -1,81 +1,12 @@
 require 'pry'
 
-def numeric?(value, numbers)
-  numbers << value if value.to_i.to_s == value || value.to_f.to_s == value
+def enter_value
+  puts 'Enter a number OR word: '
+  value = gets.chomp
 end
+#
+#
 
-def string?(value, words)
-  
-end
-
-# class String
-#   def is_string?
-#     self == self
-#     return true
-#   end
-#
-#   def is_float?
-#     self.to_f.to_s == self
-#     return true
-#   end
-#
-#   def is_integer?
-#     self.to_i.to_s == self
-#     return true
-#   end
-#
-#   def is_numeric?
-#     is_float? || is_integer?
-#     return true
-#   end
-# end
-#
-# class Float
-#   def sqrt
-#     Math.sqrt(self)
-#   end
-# end
-#
-# def is_even?
-#   return true
-# end
-#
-# def get_sum(numbers)
-#   total = 0
-#   numbers.each do |number|
-#     total += number
-#   end
-#   total
-# end
-#
-# def get_average(numbers)
-#   get_sum(numbers) / numbers.count
-# end
-#
-# def get_mode(numbers)
-#   hash = Hash.new(0)
-#   numbers.each do |frequent|
-#     hash[frequent] += 1
-#   end
-#   return hash
-# end
-#
-# def get_median(numbers)
-#   sorted = numbers.sort
-#   middle = numbers.length
-#   if middle % 2 != 0
-#     (middle + 1) / 2.0
-#   else
-#     (((middle.length/2.0) + ((middle.length + 1)/2.0)) / 2.0)
-#   end
-#   return middle
-# end
-#
-# def get_standard_diviation(numbers)
-#   variance = numbers.sort
-#   standard_diviation = Math.sqrt(variance.max - variance.min)
-#   return standard_diviation
-# end
 #
 # def try_again()
 #   puts "Invalid input. Would you like to try again?"
@@ -88,15 +19,24 @@ end
 #   end
 # end
 #
-# def main()
-#   elements = []
-#   puts "Enter a number OR a 'string' value: "
-#   user_input = gets.chomp
-#     if (user_input.to_i.to_s == user_input) || (user_input.to_f.to_s == user_input)
-#       elements << user_input.to_f
-#     else
-#       elements << user_input
-#     end
+
+def main
+  array = []
+  loop do
+    value = enter_value
+    break if value == ''
+    Input.numeric?(value, array)
+    Input.string?(value, array)
+  end
+  #TODO: try again method if both words and numbers were entered into the array
+  #TODO: method to calculate numeric statistics
+  #TODO: method to calculate string statistics
+  sum = calculate_sum(input)
+  average = sum / input.count
+  input = print_statistics(input, sum, average)
+end
+
+#
 #
 #   loop do
 #     puts "Enter a number OR a 'string' value: "
@@ -120,7 +60,7 @@ end
 #           puts elements.length.min
 #           puts elements.length.max
 #           puts elements.length / elements.count
-#         # The number of times the letter e was used in the inputs.
+#         #TODO: The number of times the letter e was used in the inputs.
 #           exit
 #         else
 #           puts "Count: #{elements.count}"
