6f30579076cc1eba7305207367c06e66e94b8fe1
Kristine <kteich88@gmail.com>
2017-01-22 12:07:09 -0500

    Updated boolean for string? to remove duplicate values.

--- lib/input.rb
+++ lib/input.rb
@@ -1,7 +1,7 @@
 # Determine type of user input
 class Input
   def self.string?(value, array)
-    array << value if value.to_i.to_s != value || value.to_f.to_s != value
+    array << value if value.to_i.to_s != value && value.to_f.to_s != value
   end
 
   def self.float?(value)
