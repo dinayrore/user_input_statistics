8daa4a8cab6d91573f350f0213073a12fb5a102a
Kristine <kteich88@gmail.com>
2017-01-22 11:47:24 -0500

    Added attr_reader so that all variables identified within the class can be accessed outside of the class and renamed a few variables to match statistic.

--- lib/numeric_statistic.rb
+++ lib/numeric_statistic.rb
@@ -1,32 +1,43 @@
 # Determine numberic statistics
-class NumericStatistic  
-  def calculate_sum(array)
+class NumericStatistic
+  attr_reader :array, :numeric_count, :sum, :average,
+              :mode, :median, :standard_diviation
+
+  def initialize(array)
+    @array = array
+  end
+
+  def calculate_count
+    numeric_count = value.count
+  end
+
+  def calculate_sum
     sum = 0
     array.each { |num| sum += num.to_f }
     sum = sum.round(2)
   end
 
-  def calculate_average(array)
+  def calculate_average
     average = calculate_sum(array) / array.count
   end
 
-  def calculate_mode(array)
+  def calculate_mode
     mode = {}
     array.each { |num| mode[num] += 1 }
   end
 
-  def calculate_median(array)
-    middle = array.sort
+  def calculate_median
+    median = array.sort
     if Integer.even?(array)
-      (((middle.length/2.0) + ((middle.length + 1)/2.0)) / 2.0)
+      (((median.length/2.0) + ((median.length + 1)/2.0)) / 2.0)
     else
-      (middle + 1) / 2.0
+      (median + 1) / 2.0
     end
-    middle
+    median
   end
 
-  def calculate_standard_diviation(numbers)
-    variance = numbers.sort
+  def calculate_standard_diviation
+    variance = array.sort
     standard_diviation = Math.sqrt(variance.max - variance.min)
   end
   standard_diviation
