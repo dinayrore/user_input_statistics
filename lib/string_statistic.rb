60ef2b9188d76ee2815ef7881ef4da39b8f01750
Kristine <kteich88@gmail.com>
2017-01-22 15:24:46 -0500

    Edited top level comment and value variable to array.

--- lib/string_statistic.rb
+++ lib/string_statistic.rb
@@ -1,29 +1,29 @@
-# Determine numberic statistics
+# Determine string statistics
 class StringStatistic
 attr_reader :value, :string_count, :minimum_string_length,
             :maximum_string_length, :average_string_length, :common_letters
 
-  def initialize(value)
-    @value =  value
+  def initialize(array)
+    @array=  array
   end
 
   def calculate_count
-    string_count = value.count
+    string_count = array.count
   end
 
   def calculate_minimum_length
-    minimum_string_length = value.length.min
+    minimum_string_length = array.length.min
   end
 
   def calculate_maximum_length
-    maximum_string_length = value.length.max
+    maximum_string_length = array.length.max
   end
 
   def calculate_average_length
-    average_string_length = value.length / value.count
+    average_string_length = array.length / array.count
   end
 
   def calculate_commonalities
-    common_letters = value.count('e')
+    common_letters = array.count('e')
   end
 end
