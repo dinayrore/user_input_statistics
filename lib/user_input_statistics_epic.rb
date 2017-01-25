839daade9e6ae5c9aa10327bd2f86172bf6d6394
Kristine <kteich88@gmail.com>
2017-01-25 17:46:24 -0500

    Updated method to verify values and execute try_again method.

--- lib/user_input_statistics_epic.rb
+++ lib/user_input_statistics_epic.rb
@@ -8,6 +8,14 @@ def enter_value
   value = gets.chomp
 end
 
+def verify_values(array)
+  if array.all? do |value|
+    Input.float?(value) || Input.integer?(value) && value == String
+    try_again
+    end
+  end
+end
+
 def print_string_statistics
   puts "Count: #{string_count}"
   puts "Minimum String Length: #{minimum_string_length}"
@@ -26,16 +34,17 @@ def print_numeric_statistics
   puts "Standard Deviation: #{standard_diviation}"
 end
 
-# def try_again()
-#   puts "Invalid input. Would you like to try again?"
-#   print "Type \'retry\' to try again or \'exit\' to return to the Terminal: "
-#   input = gets.chomp.downcase
-#   if input == "retry"
-#     main
-#   else
-#     exit
-#   end
-# end
+def try_again()
+  puts 'Your data set contains both numeric values and strings.'
+  puts 'This input is invalid. Would you like to try again?'
+  print "Type \'retry\' to try again or \'exit\' to return to the Terminal: "
+  input = gets.chomp.downcase
+  if input == 'retry'
+    main
+  else
+    exit
+  end
+end
 
 def main
   array = []
@@ -45,7 +54,7 @@ def main
     Input.numeric?(value, array)
     Input.string?(value, array)
   end
-  #TODO: try again method if both words and numbers were entered into the array
+  verify_values(array)
   #TODO: method to calculate numeric statistics
   #TODO: method to calculate string statistics
 end
