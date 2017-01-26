d40e422eeb4d228aa54da557a3bdf172b0abe097
Kristine <kteich88@gmail.com>
2017-01-25 20:06:52 -0500

    Created tests for .float? and .integer? methods.

--- spec/input_spec.rb
+++ spec/input_spec.rb
@@ -8,7 +8,34 @@ describe Input do
       end
     end
   end
-
+  describe '.float?' do
+    context "given '3.45'" do
+      it 'returns true' do
+        expect(Input.float?('3.45')).to be true
+      end
+    end
+  end
+  describe '.float?' do
+    context "given '-1.23'" do
+      it 'returns true' do
+        expect(Input.float?('-1.23')).to be true
+      end
+    end
+  end
+  describe '.integer?' do
+    context "given '42'" do
+      it 'returns true' do
+        expect(Input.integer?('42')).to be true
+      end
+    end
+  end
+  describe '.integer?' do
+    context "given '-9'" do
+      it 'returns true' do
+        expect(Input.integer?('-9')).to be true
+      end
+    end
+  end
   describe '.numeric?' do
     context "given '42', []" do
       it "returns ['42']" do
