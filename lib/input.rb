dc0332aa510b52af247a1c78c9e22a6dcbfa9764
Kristine <kteich88@gmail.com>
2017-01-26 22:58:43 -0500

    Rewrote class methods to return boolean values only.

--- lib/input.rb
+++ lib/input.rb
@@ -1,21 +1,20 @@
 # Determine type of user input
 class Input
-  attr_reader :value, :array
+  attr_reader :value
 
-  def initialize(value, array)
+  def initialize(value)
     @value = value
-    @array = array  
   end
 
-  def self.string?(value, array)
-    array << value if value.to_i.to_s != value && value.to_f.to_s != value
+  def self.string?
+    return true if value.to_i.to_s != value && value.to_f.to_s != value
   end
 
-  def self.float?(value, array)
-    array << value.to_f if value.to_f.to_s == value
+  def self.float?
+    return true if value.to_f.to_s == value
   end
 
-  def self.integer?(value, array)
-    array << value.to_i if value.to_i.to_s == value
+  def self.integer?
+    return true if value.to_i.to_s == value
   end
 end
