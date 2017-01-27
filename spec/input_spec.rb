d3abe50c1647bdc08c8e87d166ef78c2d1736a77
Kristine <kteich88@gmail.com>
2017-01-26 23:00:06 -0500

    Updated tests, tests are unfinished for newest version of boolean only response.

--- spec/input_spec.rb
+++ spec/input_spec.rb
@@ -1,60 +1,41 @@
 require 'input.rb'
 
 describe Input do
+  before(:context) do
+    value = Input.new
+  end
   describe '.string?' do
-    context "given 'kitten', []" do
-      it "returns ['kitten']" do
-        expect(Input.string?('kitten', [])).to eq(['kitten'])
+    context "given value = Input.new('kitten')" do
+      it 'returns true' do
+        expect(value.string?).to be true
       end
     end
   end
   describe '.float?' do
     context "given '3.45'" do
-      it 'returns true' do
-        expect(Input.float?('3.45')).to be true
+      it 'returns true'  do
+        expect(Input.new('3.45').float?).to be true
       end
     end
   end
   describe '.float?' do
     context "given '-1.23'" do
-      it 'returns true' do
-        expect(Input.float?('-1.23')).to be true
+      it 'returns true'  do
+        expect(Input.new('-1.23').float?).to be true
       end
     end
   end
   describe '.integer?' do
     context "given '42'" do
       it 'returns true' do
-        expect(Input.integer?('42')).to be true
+        expect(Input.new('42').integer?).to be true
       end
     end
   end
   describe '.integer?' do
     context "given '-9'" do
       it 'returns true' do
-        expect(Input.integer?('-9')).to be true
-      end
-    end
-  end
-  describe '.numeric?' do
-    context "given '42', []" do
-      it "returns ['42']" do
-        expect(Input.numeric?('42', [])).to eq(['42'])
-      end
-    end
-    context "given '3.45', []" do
-      it "returns ['3.45']" do
-        expect(Input.numeric?('3.45', [])).to eq(['3.45'])
-      end
-    end
-    context "given '-9', []" do
-      it "returns ['-9']" do
-        expect(Input.numeric?('-9', [])).to eq(['-9'])
-      end
-    end
-    context "given '-1.23', []" do
-      it "returns ['-1.23']" do
-        expect(Input.numeric?('-1.23', [])).to eq(['-1.23'])
+        expect(Input.new('-9').integer?).to be true
       end
     end
   end
