d36dafa16510c5e592ee4fd9b97a55662a59b3c7
Kristine <kteich88@gmail.com>
2017-01-27 15:28:14 -0500

    Modified tests to boolean statements.  All tests passing.

--- spec/input_spec.rb
+++ spec/input_spec.rb
@@ -1,41 +1,45 @@
 require 'input.rb'
 
 describe Input do
-  before(:context) do
-    value = Input.new
-  end
   describe '.string?' do
-    context "given value = Input.new('kitten')" do
+    context "given 'kitten'" do
       it 'returns true' do
+        value = Input.new('kitten')
         expect(value.string?).to be true
       end
     end
   end
+
   describe '.float?' do
     context "given '3.45'" do
-      it 'returns true'  do
-        expect(Input.new('3.45').float?).to be true
+      it 'returns true' do
+        value = Input.new('3.45')
+        expect(value.float?).to be true
       end
     end
   end
   describe '.float?' do
     context "given '-1.23'" do
-      it 'returns true'  do
-        expect(Input.new('-1.23').float?).to be true
+      it 'returns true' do
+        value = Input.new('-1.23')
+        expect(value.float?).to be true
       end
     end
   end
+
   describe '.integer?' do
     context "given '42'" do
       it 'returns true' do
-        expect(Input.new('42').integer?).to be true
+        value = Input.new('42')
+        expect(value.integer?).to be true
       end
     end
   end
   describe '.integer?' do
     context "given '-9'" do
       it 'returns true' do
-        expect(Input.new('-9').integer?).to be true
+        value = Input.new('-9')
+        expect(value.integer?).to be true
       end
     end
   end
