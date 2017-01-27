40f2e0b11505c06c3afc91aa279dc2907522a47c
Kristine <kteich88@gmail.com>
2017-01-26 21:00:00 -0500

    Removed numeric class method and added initialize.

--- lib/input.rb
+++ lib/input.rb
@@ -1,18 +1,21 @@
 # Determine type of user input
 class Input
+  attr_reader :value, :array
+
+  def initialize(value, array)
+    @value = value
+    @array = array  
+  end
+
   def self.string?(value, array)
     array << value if value.to_i.to_s != value && value.to_f.to_s != value
   end
 
-  def self.float?(value)
-    return true if value.to_f.to_s == value
+  def self.float?(value, array)
+    array << value.to_f if value.to_f.to_s == value
   end
 
-  def self.integer?(value)
-    return true if value.to_i.to_s == value
-  end
-
-  def self.numeric?(value, array)
-    array << value if float?(value) || integer?(value)
+  def self.integer?(value, array)
+    array << value.to_i if value.to_i.to_s == value
   end
 end
