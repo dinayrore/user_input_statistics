d120353a559eb0e342db7d624a2bcb15aa69da69
Kristine <kteich88@gmail.com>
2017-01-28 15:10:51 -0500

    Revised code to pass tests.

--- lib/string_statistic.rb
+++ lib/string_statistic.rb
@@ -1,26 +1,26 @@
-# Determine string statistics
+# Calculate string statistics
 class StringStatistic
   def initialize(array)
     @array = array
   end
 
   def calculate_count
-    array.count
+    @array.count
   end
 
   def calculate_minimum_length
-    array.min_by(&:length)
+    @array.min_by(&:size).length
   end
 
   def calculate_maximum_length
-    array.max_by(&:length)
+    @array.max_by(&:size).length
   end
 
   def calculate_average_length
-    array.length / array.count
+    @array.join.length.to_f / @array.length
   end
 
   def calculate_commonalities
-    array.join.count 'e'
+    @array.join.count 'e'
   end
 end
